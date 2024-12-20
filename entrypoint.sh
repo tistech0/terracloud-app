fca06c0efa82b96a27f2bb4fe277c01f1584d04b
Administrator <gitlab_admin_b5a294@example.com>
2024-12-20 17:02:49 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,17 @@
 # Attendre que les variables d'environnement soient appliquées
 dockerize -template /var/www/html/.env.tmpl:/var/www/html/.env
 
-# Générer la clé d'application Laravel sans confirmation
+# Créer les répertoires nécessaires
+mkdir -p /var/www/html/bootstrap/cache
+mkdir -p /var/www/html/storage/framework/cache
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/framework/views
+
+# Définir les permissions
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+# Générer la clé d'application Laravel
 APP_KEY=$(php artisan key:generate --show)
 echo "APP_KEY=$APP_KEY" >> /var/www/html/.env
 
