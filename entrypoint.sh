d52b6aa225a3f9094bbcd42c2fc5b11ce8fd65e7
Administrator <gitlab_admin_b5a294@example.com>
2024-12-20 15:57:14 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # Attendre que les variables d'environnement soient appliquées
 dockerize -template /var/www/html/.env.tmpl:/var/www/html/.env
 
-# Générer la clé d'application Laravel
-php artisan key:generate
+# Générer la clé d'application Laravel sans confirmation
+php artisan key:generate --force
 
 # Lancer Apache
 exec apache2-foreground
