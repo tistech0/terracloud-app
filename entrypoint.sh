80d6f968ab5cfdbb704dec219320585617eec975
Administrator <gitlab_admin_b5a294@example.com>
2024-12-20 16:07:12 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,24 @@
 # Attendre que les variables d'environnement soient appliquées
 dockerize -template /var/www/html/.env.tmpl:/var/www/html/.env
 
-# Générer la clé d'application Laravel sans confirmation
-php artisan key:generate --force
+# Vérifier si une clé Laravel existe déjà dans le fichier .env
+if grep -q "^APP_KEY=" /var/www/html/.env; then
+    echo "APP_KEY déjà défini, pas besoin de le régénérer."
+else
+    # Générer une nouvelle clé d'application Laravel sans confirmation
+    APP_KEY=$(php artisan key:generate --show)
+    if [ -n "$APP_KEY" ]; then
+        echo "APP_KEY=$APP_KEY" >> /var/www/html/.env
+        echo "Clé d'application Laravel générée : $APP_KEY"
+    else
+        echo "Erreur : Impossible de générer APP_KEY !" >&2
+        exit 1
+    fi
+fi
+
+# Vérifier les permissions sur le fichier .env
+chown www-data:www-data /var/www/html/.env
+chmod 644 /var/www/html/.env
 
 # Lancer Apache
 exec apache2-foreground
